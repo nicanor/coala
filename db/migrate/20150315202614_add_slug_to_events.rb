04514b6d4a002b558ec27340271f9e13c067b811
Fede Otaran <federico.otaran@cespi.unlp.edu.ar>
2015-04-02 17:26:38 -0300

    Add unique to slug

--- db/migrate/20150315202614_add_slug_to_events.rb
+++ db/migrate/20150315202614_add_slug_to_events.rb
@@ -1,5 +1,5 @@
 class AddSlugToEvents < ActiveRecord::Migration
   def change
-    add_column :events, :slug, :string, null: false
+    add_column :events, :slug, :string, null: false, unique: true
   end
 end
